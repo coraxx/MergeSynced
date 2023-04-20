887c200b7b706d0eb9ae023f049f3d6d6f940eae
Jan Arnold <jan.arnold@coraxx.net>
2023-04-21 01:08:03 +0200

    Adjustments to mac release script

--- build/release.sh
+++ build/release.sh
@@ -9,6 +9,15 @@ SIGNING_IDENTITY="Jan Arnold"
 SELECTED_PLATFORM=""
 
 signCode () {
+    find "$APP_NAME_BASE/"|while read fname; do
+        if [[ -f $fname ]]; then
+            echo "[INFO] Signing $fname"
+            codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$fname"
+        fi
+    done
+}
+
+signBundle () {
     find "$APP_NAME/Contents/MacOS/"|while read fname; do
         if [[ -f $fname ]]; then
             echo "[INFO] Signing $fname"
@@ -33,6 +42,7 @@ compile () {
     dotnet clean $PROJECT_PATH
     rm -rf "$APP_NAME_BASE"
     rm -rf "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip"
+    rm -rf "${APP_NAME_BASE}_$SELECTED_PLATFORM.dmg"
 
     echo Building for $SELECTED_PLATFORM...
     echo "dotnet publish $PROJECT_PATH -p:RuntimeIdentifier=$SELECTED_PLATFORM -p:Configuration=Release"
@@ -41,19 +51,41 @@ compile () {
     echo Copy $APP_NAME_BASE and rename folder
     cp -R $BIN_RELEASE_PATH/$SELECTED_PLATFORM/publish "$APP_NAME_BASE"
 
+    # Check if packing mac version to copy image and start signing
     if test -f "$APP_NAME_BASE/libAvaloniaNative.dylib"; then
-        echo Adding icon to executable
-        sips -i "$APP_NAME_BASE/Assets/MergeSyncedLogoSimple.icns"
-        DeRez -only icns "$APP_NAME_BASE/Assets/MergeSyncedLogoSimple.icns" > tmpicns.rsrc
-        Rez -append tmpicns.rsrc -o "$APP_NAME_BASE/$APP_NAME_BASE"
-        SetFile -a C "$APP_NAME_BASE/$APP_NAME_BASE"
-        rm -rf tmpicns.rsrc
+        # Signing not possible with icon
+        # echo Adding icon to executable
+        # sips -i "$APP_NAME_BASE/Assets/MergeSyncedLogoSimple.icns"
+        # DeRez -only icns "$APP_NAME_BASE/Assets/MergeSyncedLogoSimple.icns" > tmpicns.rsrc
+        # Rez -append tmpicns.rsrc -o "$APP_NAME_BASE/$APP_NAME_BASE"
+        # SetFile -a C "$APP_NAME_BASE/$APP_NAME_BASE"
+        # rm -rf tmpicns.rsrc
+        rm -rf "$APP_NAME_BASE/Assets" # delete before signing
+        # Sign files
+        signCode
     fi
     rm -rf "$APP_NAME_BASE/Assets"
 
     echo Create release zip...
     if test -f "$APP_NAME_BASE/libAvaloniaNative.dylib"; then
-        ditto -c -k --sequesterRsrc --keepParent "$APP_NAME_BASE" "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip"
+        #ditto -c -k --sequesterRsrc --keepParent "$APP_NAME_BASE" "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip"
+        # Create root folder for dmg
+        mkdir dmgroot
+        mv $APP_NAME_BASE dmgroot/
+        mv dmgroot $APP_NAME_BASE
+        create-dmg \
+            --volname "$APP_NAME_BASE" \
+            --volicon "../MergeSynced/Assets/MergeSyncedLogoSimple.icns" \
+            --window-pos 200 120 \
+            --window-size 800 400 \
+            --icon-size 100 \
+            --app-drop-link 600 185 \
+            --codesign "Jan Arnold" \
+            --notarize "AC_PASSWORD" \
+            "${APP_NAME_BASE}_$SELECTED_PLATFORM.dmg" \
+            "$APP_NAME_BASE"
+        # Ask if zip should be notarized
+        #notarize
     else
         zip -r "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip" "$APP_NAME_BASE" -x '**/.*' -x '**/__MACOSX'
     fi
@@ -80,7 +112,7 @@ compileBundle () {
     echo Clean up pre compile...
     dotnet clean $PROJECT_PATH
     rm -rf "$APP_NAME"
-    rm -rf "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip"
+    rm -rf "${APP_NAME_BASE}_$SELECTED_PLATFORM.dmg"
 
     echo Building for $SELECTED_PLATFORM...
     echo "dotnet publish $PROJECT_PATH -t:BundleApp -p:RuntimeIdentifier=$SELECTED_PLATFORM -p:Configuration=Release"
@@ -90,11 +122,24 @@ compileBundle () {
     cp -R $BIN_RELEASE_PATH/$SELECTED_PLATFORM/publish/$APP_NAME "$APP_NAME"
 
     echo Start code signing...
-    signCode
+    signBundle
 
-    echo Create release zip...
-    ditto -c -k --sequesterRsrc --keepParent "$APP_NAME" "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip"
-    notarize
+    echo Create release dmg...
+    #ditto -c -k --sequesterRsrc --keepParent "$APP_NAME" "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip"
+    create-dmg \
+        --volname "$APP_NAME_BASE" \
+        --volicon "../MergeSynced/Assets/MergeSyncedLogoSimple.icns" \
+        --background "DmgBg.png" \
+        --window-pos 200 120 \
+        --window-size 550 400 \
+        --icon-size 100 \
+        --icon "$APP_NAME" 100 170 \
+        --hide-extension "$APP_NAME" \
+        --app-drop-link 400 170 \
+        --codesign "Jan Arnold" \
+        --notarize "AC_PASSWORD" \
+        "${APP_NAME_BASE}_$SELECTED_PLATFORM.dmg" \
+        "$APP_NAME"
 
     echo Cleanup for $SELECTED_PLATFORM...
     rm -rf $APP_NAME
@@ -106,7 +151,7 @@ notarize (){
     while true; do
         read -p "Do you wish to notarize ${APP_NAME_BASE}_$SELECTED_PLATFORM.zip? " yn
         case $yn in
-            [Yy]* ) xcrun altool --notarize-app -f "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip" --primary-bundle-id com.coraxx.mergesynched-$SELECTED_PLATFORM -u "@keychain:AC_PASSWORD" -p "@keychain:AC_PASSWORD"; break;;
+            [Yy]* ) xcrun notarytool submit "${APP_NAME_BASE}_$SELECTED_PLATFORM.zip" --keychain-profile "AC_PASSWORD"; break;;
             [Nn]* ) echo "Skipping..."; break;;
             * ) echo "Please answer yes or no.";;
         esac
@@ -121,12 +166,12 @@ notarize (){
 echo Building $APP_NAME for MacOs X...
 ## x64 ###########################################
 SELECTED_PLATFORM="osx-x64"
-compile
+compileBundle
 #read -p "Press enter to continue"
 
 ## arm64 #########################################
 SELECTED_PLATFORM="osx-arm64"
-compile
+compileBundle
 #read -p "Press enter to continue"
 
 ##################################################
